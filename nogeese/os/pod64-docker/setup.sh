2897843fe29733d62cd7d310d731b3de88f906a8
Nogeese <leon8326nogeese@outlook.com>
2025-03-15 09:45:18 +0000

    Update setup.sh

--- nogeese/os/pod64-docker/setup.sh
+++ nogeese/os/pod64-docker/setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sudo bash
 cd /etc
-sudo echo "" >> pacman.conf
-sudo echo "# Nogeese Repositories" >> pacman.conf
-sudo echo "[nogeese]" >> pacman.conf
-sudo echo "Server = https://raw.githubusercontent.com/nogeese-org/mirror/stable/nogeese/os/pod64-docker"
-sudo echo "SigLevel = Optional TrustAll"
+{
+    echo ""
+    echo "# Nogeese Repositories"
+    echo "[nogeese]"
+    echo "Server = https://raw.githubusercontent.com/nogeese-org/mirror/stable/nogeese/os/pod64-docker"
+    echo "SigLevel = Optional TrustAll"
+} | sudo tee -a pacman.conf
