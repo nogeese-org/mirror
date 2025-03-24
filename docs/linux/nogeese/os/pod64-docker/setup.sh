670a14c9cce1205a1a1bbbf0bef8598a1608718f
Nogeese <leoszen@aol.com>
2025-03-24 12:50:27 +0000

    Delete docs/linux directory

--- docs/linux/nogeese/os/pod64-docker/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sudo bash
-cd /etc
-{
-    echo ""
-    echo "# Nogeese Repositories"
-    echo "[nogeese]"
-    echo "Server = https://raw.githubusercontent.com/nogeese-org/mirror/stable/nogeese/os/pod64-docker"
-    echo "SigLevel = Optional TrustAll"
-} | sudo tee -a pacman.conf
